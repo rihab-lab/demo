8be8d34a6adf3a734d9b5fd8e14519384724cbcc
rihab-lab <rihab.bahri7@gmail.com>
2025-04-11 16:25:26 +0200

    fix: configchange05

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,5 +1,5 @@
-resource "snowflake_database" "demo_db" {
-  name    = "DEV_POC_VISEO_DB"
+resource "snowflake_database" "db" {
+  name    = "Database_POC_VISEO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
 
@@ -9,18 +9,18 @@ resource "snowflake_grant_privileges_to_account_role" "database_grant" {
   account_role_name = snowflake_account_role.role.name
   on_account_object {
     object_type = "DATABASE"
-    object_name = snowflake_database.demo_db.name
+    object_name = snowflake_database.db.name
   }
 }
 
 resource "snowflake_schema" "raw_layer" {
   provider            = snowflake.sys_admin       // Utilise le provider aliasé "sys_admin"
-  database            = snowflake_database.demo_db.name  // La base de données à laquelle se rattache le schéma
+  database            = snowflake_database.db.name  // La base de données à laquelle se rattache le schéma
   name                = "RAW_LAYER"
   with_managed_access = false
   
   depends_on = [
-    snowflake_database.demo_db,
+    snowflake_database.db,
     snowflake_grant_privileges_to_account_role.database_grant
   ]
 }
@@ -33,6 +33,6 @@ resource "snowflake_grant_privileges_to_account_role" "schema_grant_raw" {
   
   on_schema {
     // Construction du nom complet du schéma : "DATABASE"."SCHEMA"
-    schema_name = "\"${snowflake_database.demo_db.name}\".\"${snowflake_schema.raw_layer.name}\""
+    schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.raw_layer.name}\""
   }
 }
