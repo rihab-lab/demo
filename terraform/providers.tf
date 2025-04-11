f0c12ae5bc0b36e15dba6423755fa372a15c6b55
rihab-lab <rihab.bahri7@gmail.com>
2025-04-11 15:25:21 +0200

    fix: change providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
-
 provider "snowflake" {
+  alias                    = "security_admin"
+  role                     = "SECURITYADMIN"
+  preview_features_enabled = ["snowflake_table_resource"] 
+ 
 }
 
+provider "snowflake" {
+  alias                    = "sys_admin"
+  role                     = "SYSADMIN"
+  preview_features_enabled = ["snowflake_table_resource"] 
+}
