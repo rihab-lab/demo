410f1ed2416203275462eaeedd89a6e37cb2e7d0
rihab-lab <rihab.bahri7@gmail.com>
2025-04-11 16:42:18 +0200

    fix: configchange0-

--- terraform/database.tf
+++ terraform/database.tf
@@ -7,6 +7,7 @@ resource "snowflake_grant_privileges_to_account_role" "database_grant" {
   provider          = snowflake.security_admin
   privileges        = ["USAGE", "MONITOR", "MODIFY", "CREATE SCHEMA"]
   account_role_name = snowflake_account_role.role.name
+
   on_account_object {
     object_type = "DATABASE"
     object_name = snowflake_database.db.name
@@ -14,25 +15,36 @@ resource "snowflake_grant_privileges_to_account_role" "database_grant" {
 }
 
 resource "snowflake_schema" "raw_layer" {
-  provider            = snowflake.sys_admin       // Utilise le provider aliasé "sys_admin"
-  database            = snowflake_database.db.name  // La base de données à laquelle se rattache le schéma
+  provider            = snowflake.sys_admin       // Utilisation du provider sys_admin pour la création du schéma
+  database            = snowflake_database.db.name  // Référence à la base créée ci-dessus
   name                = "RAW_LAYER"
   with_managed_access = false
-  
+
   depends_on = [
     snowflake_database.db,
     snowflake_grant_privileges_to_account_role.database_grant
   ]
 }
 
-
 resource "snowflake_grant_privileges_to_account_role" "schema_grant_raw" {
-  provider          = snowflake.security_admin    // Utilise le provider aliasé "security_admin"
-  privileges        = ["USAGE", "CREATE EXTERNAL TABLE", "CREATE TABLE", "CREATE VIEW", "CREATE PROCEDURE", "CREATE NOTEBOOK", "CREATE STAGE", "CREATE FILE FORMAT", "CREATE TASK", "CREATE STREAM", "CREATE PIPE"]
-  account_role_name = snowflake_account_role.role.name  // Nom du rôle auquel accorder les privilèges
-  
+  provider          = snowflake.security_admin    // Provider pour attribuer les grants sur le schéma
+  privileges        = [
+    "USAGE",
+    "CREATE EXTERNAL TABLE",
+    "CREATE TABLE",
+    "CREATE VIEW",
+    "CREATE PROCEDURE",
+    "CREATE NOTEBOOK",
+    "CREATE STAGE",
+    "CREATE FILE FORMAT",
+    "CREATE TASK",
+    "CREATE STREAM",
+    "CREATE PIPE"
+  ]
+  account_role_name = snowflake_account_role.role.name
+
   on_schema {
-    // Construction du nom complet du schéma : "DATABASE"."SCHEMA"
-    schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.raw_layer.name}\""
+    // Construction du nom complet du schéma sous la forme Database.Schema
+    schema_name = "${snowflake_database.db.name}.${snowflake_schema.raw_layer.name}"
   }
 }
