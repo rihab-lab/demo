608eb8e7309ad285bee191967c271cb566e85768
rihab-lab <rihab.bahri7@gmail.com>
2025-04-15 14:34:20 +0200

    fix: errors configs pipe and stage6

--- terraform/stage.tf
+++ terraform/stage.tf
@@ -1,30 +1,29 @@
+#################################################
 # File Format
-################################
+#################################################
 resource "snowflake_file_format" "format" {
   provider  = snowflake.sys_admin
   name      = "CSV_FORMAT"
-  database  = snowflake_database.db.name       # "TEST_POC_VISEO_DB"
-  schema    = snowflake_schema.raw_layer.name  # "RAW_LAYER"
-  format_type     = "CSV"
+  database  = snowflake_database.db.name       # e.g., "TEST_POC_VISEO_DB"
+  schema    = snowflake_schema.raw_layer.name  # e.g., "RAW_LAYER"
+  format_type = "CSV"
 
-  # CSV-specific options
   skip_header                  = 1
   field_optionally_enclosed_by = "\""
 }
 
-################################
+#################################################
 # Stage
-################################
+#################################################
 resource "snowflake_stage" "azure_stage" {
-  provider  = snowflake.sys_admin
-  name      = "EXTERNAL_AZURE_STAGE"
-  database  = snowflake_database.db.name
-  schema    = snowflake_schema.raw_layer.name
+  provider = snowflake.sys_admin
+  name     = "EXTERNAL_AZURE_STAGE"
+  database = snowflake_database.db.name
+  schema   = snowflake_schema.raw_layer.name
 
-  url        = "azure://storageacctpoc.blob.core.windows.net/landing-zone?sp=r&st=2025-04-15T07:55:24Z&se=2026-06-01T15:55:24Z&spr=https&sv=2024-11-04&sr=c&sig=xxx"
+  # Fully qualified SAS URL
+  url = "azure://storageacctpoc.blob.core.windows.net/landing-zone?sp=r&st=2025-04-15T07:55:24Z&se=2026-06-01T15:55:24Z&spr=https&sv=2024-11-04&sr=c&sig=xxx"
 
-  # Référence le file format défini plus haut
-  file_format = "CSV_FORMAT"
-
- 
+  # Best practice: reference the resource instead of a plain string
+  file_format = "${snowflake_file_format.format.name}"
 }
