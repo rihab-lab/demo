8646aea13622c55a1ad668c2c5a2d19e73b59499
rihab-lab <rihab.bahri7@gmail.com>
2025-04-15 14:02:51 +0200

    fix: errors configs pipe and stage2

--- terraform/stage.tf
+++ terraform/stage.tf
@@ -24,7 +24,7 @@ resource "snowflake_stage" "azure_stage" {
   url        = "azure://storageacctpoc.blob.core.windows.net/landing-zone?sp=r&st=2025-04-15T07:55:24Z&se=2026-06-01T15:55:24Z&spr=https&sv=2024-11-04&sr=c&sig=xxx"
 
   # Référence le file format défini plus haut
-  file_format = "\"TEST_POC_VISEO_DB\".\"RAW_LAYER\".\"CSV_FORMAT\""
+  file_format = "TEST_POC_VISEO_DB.RAW_LAYER.CSV_FORMAT"
 
  
 }
