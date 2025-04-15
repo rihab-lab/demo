5d7401a997e3c82fd465b60b177a700881edad17
rihab-lab <rihab.bahri7@gmail.com>
2025-04-15 16:40:55 +0200

    fix: errors configs pipe and stagepjjzzzzzzzyyyyyyy

--- terraform/stage.tf
+++ terraform/stage.tf
@@ -28,7 +28,5 @@ resource "snowflake_stage" "azure_stage" {
   schema              = "RAW_LAYER"
   url                 = var.azure_container_url
   storage_integration = snowflake_storage_integration.azure_int.name
- file_format = {
-  format_name = "TEST_POC_VISEO_DB.RAW_LAYER.CSV_FORMAT"
-}
+  file_format         = "\"TEST_POC_VISEO_DB\".\"RAW_LAYER\".\"CSV_FORMAT\""
 }
