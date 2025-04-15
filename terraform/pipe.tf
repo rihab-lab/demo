d6e1147706b8c6921992575ab611507bb9752a08
rihab-lab <rihab.bahri7@gmail.com>
2025-04-15 12:02:31 +0200

    fix: errors configs pipe

--- terraform/pipe.tf
+++ terraform/pipe.tf
@@ -1,13 +1,13 @@
 resource "snowflake_pipe" "metadata_pipe" {
   provider    = snowflake.sys_admin
   name        = "MY_METADATA_PIPE"
-  database = snowflake_database.db.name
-  schema   = snowflake_schema.raw_layer.name
+  database = "TEST_POC_VISEO_DB"
+  schema   = "RAW_LAYER"
   comment     = "Pipe automatique pour ingérer les métadonnées depuis le stage Azure"
   auto_ingest = true
 
   copy_statement = <<-EOT
-    COPY INTO TEST_DB.RAW_LAYER.RAW_METADATA (FILE_NAME, LOAD_DATE)
+    COPY INTO TEST_POC_VISEO_DB.RAW_LAYER.RAW_METADATA (FILE_NAME, LOAD_DATE)
     FROM (
       SELECT 
         METADATA\$FILENAME AS FILE_NAME,
