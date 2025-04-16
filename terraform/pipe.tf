f83539632a25ee1e1335959f0c222362f7a08c41
rihab-lab <rihab.bahri7@gmail.com>
2025-04-16 16:13:25 +0200

    fix: pipe mm

--- terraform/pipe.tf
+++ terraform/pipe.tf
@@ -5,32 +5,32 @@ resource "snowflake_pipe" "pipe_raw_prc_benchmark" {
   schema   = "RAW_LAYER"
 
   copy_statement = <<-SQL
-    COPY INTO TEST_POC_VISEO_DB.RAW_LAYER.RAW_PRC_BENCHMARK (
-      APUKCode,
-      Anabench2Code,
-      Anabench2,
-      SKUGroup,
-      SYS_SOURCE_DATE,
-      FILE_NAME,
-      LOAD_TIME
-    )
-    FROM (
-      SELECT
-        $1,                         -- APUKCode
-        $2,                         -- Anabench2Code
-        $3,                         -- Anabench2
-        $4,                         -- SKUGroup
-        $5,                         -- SYS_SOURCE_DATE
-        METADATA$FILENAME,         -- FILE_NAME
-        CURRENT_TIMESTAMP()        -- LOAD_TIME
-      FROM @TEST_POC_VISEO_DB.RAW_LAYER.EXTERNAL_AZUR_STAGE
-    )
-    FILE_FORMAT = (
-      TYPE = 'CSV'
-      FIELD_DELIMITER = ';'
-      FIELD_OPTIONALLY_ENCLOSED_BY = '"'
-      SKIP_HEADER = 1
-    )
-    PATTERN = '.*PRC_BENCHMARK_[0-9]{8}\\.csv';
+   COPY INTO TEST_POC_VISEO_DB.RAW_LAYER.RAW_PRC_BENCHMARK (
+  "APUKCode",
+  "Anabench2Code",
+  "Anabench2",
+  "SKUGroup",
+  SYS_SOURCE_DATE,
+  FILE_NAME,
+  LOAD_TIME
+)
+FROM (
+  SELECT
+    $1,                         -- APUKCode
+    $2,                         -- Anabench2Code
+    $3,                         -- Anabench2
+    $4,                         -- SKUGroup
+    $5,
+    METADATA$FILENAME,         -- FILE_NAME
+    CURRENT_TIMESTAMP()        -- LOAD_TIME
+  FROM @TEST_POC_VISEO_DB.RAW_LAYER.EXTERNAL_AZUR_STAGE
+)
+FILE_FORMAT = (
+  TYPE = 'CSV'
+  FIELD_DELIMITER = ';'
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+  SKIP_HEADER = 1
+)
+PATTERN = '.*PRC_BENCHMARK_[0-9]{8}\\.csv';
   SQL
 }
