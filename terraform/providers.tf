dbf62e9c77851e5720ab1ee543c0fe7d68b22146
rihab-lab <rihab.bahri7@gmail.com>
2025-04-16 16:09:48 +0200

    fix: pipe

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,7 +14,7 @@ provider "snowflake" {
   alias                    = "account_admin"
   role                     = "ACCOUNTADMIN"
   preview_features_enabled = ["snowflake_table_resource","snowflake_storage_integration_resource",
-    "snowflake_file_format_resource", "snowflake_stage_resource"] # mandatory to use preview resource "table creation"
+    "snowflake_file_format_resource", "snowflake_stage_resource","snowflake_pipe_resource"] # mandatory to use preview resource "table creation"
 }
 
 
