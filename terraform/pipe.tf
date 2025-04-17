1bfe252c5b4d034a32669e401ec954cfdcdfe5e7
rihab-lab <rihab.bahri7@gmail.com>
2025-04-17 15:03:56 +0200

    fix: change config

--- terraform/pipe.tf
+++ terraform/pipe.tf
@@ -5,14 +5,14 @@ resource "snowflake_pipe" "pipe_raw_prc_benchmark" {
   schema   = "RAW_LAYER"
 
   copy_statement = <<-SQL
-   COPY INTO TEST_POC_VISEO_DB.RAW_LAYER.RAW_PRC_BENCHMARK (
-  "APUKCode",
-  "Anabench2Code",
-  "Anabench2",
-  "SKUGroup",
-   SYS_SOURCE_DATE,
-   FILE_NAME,
-   LOAD_TIME
+  COPY INTO TEST_POC_VISEO_DB.RAW_LAYER.PRC_BENCHMARK_RAW (
+  APUKCODE ,
+  ANABENCH2CODE,
+  ANABENCH2 ,
+  SKUGROUP ,
+  SYS_SOURCE_DATE ,
+  FILE_NAME ,
+  LOAD_TIME 
 )
 FROM (
   SELECT
@@ -34,3 +34,49 @@ FILE_FORMAT = (
 PATTERN = '.*PRC_BENCHMARK_[0-9]{8}\\.csv';
   SQL
 }
+
+
+resource "snowflake_pipe" "pipe_raw_prc_campaign" {
+  name     = "PIPE_RAW_PRC_CAMPAIGN"
+  provider = snowflake.account_admin
+  database = "TEST_POC_VISEO_DB"
+  schema   = "RAW_LAYER"
+
+  copy_statement = <<-SQL
+  COPY INTO TEST_POC_VISEO_DB.RAW_LAYER.PRC_CAMPAIGN_RAW(
+    HOUSEKEY ,
+	CAMPAIGNCODE ,
+	CAMPAIGNNAME ,
+	CAMPAIGNDESCRIPTION ,
+	HISTORICALSELLINFIRSTMONTH ,
+	HISTORICALSELLINLASTMONTH ,
+	CAMPAIGNDATE ,
+	SYS_SOURCE_DATE ,
+	FILE_NAME ,
+	LOAD_TIME 
+)
+FROM (
+  SELECT
+    $1, 
+    $2,                         
+    $3,                      
+    $4,                         
+    $5,
+    $6,
+    $7,
+    $8,
+    METADATA$FILENAME,         -- FILE_NAME
+    CURRENT_TIMESTAMP()        -- LOAD_TIME
+  FROM @TEST_POC_VISEO_DB.RAW_LAYER.EXTERNAL_AZUR_STAGE
+)
+FILE_FORMAT = (
+  TYPE = 'CSV'
+  FIELD_DELIMITER = ';'
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+  SKIP_HEADER = 1
+)
+PATTERN = '.*PRC_CAMPAIGN_[0-9]{8}\\.csv';
+  SQL
+}
+
+
