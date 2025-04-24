ea6887e1ddb2be7969cdfa2e330f3d35aee379b2
rihab-lab <rihab.bahri7@gmail.com>
2025-04-24 15:29:47 +0200

    feat: deploy infra snowfalke data

--- terraform/stage.tf
+++ terraform/stage.tf
@@ -10,24 +10,17 @@ resource "snowflake_storage_integration" "azure_int" {
   enabled = true
 }
 
-resource "snowflake_file_format" "csv_format" {
-  provider              = snowflake.account_admin
-  name        = "CSV_FORMAT"
-  database    = "TEST_POC_VISEO_DB"
-  schema      = "RAW_LAYER"
-  format_type = "CSV"
-
-  skip_header                   = 1
-  field_optionally_enclosed_by = "\""
-}
-
 resource "snowflake_stage" "azure_stage" {
   provider            = snowflake.account_admin
   name                = "EXTERNAL_AZURE_STAGE"
   database            = "TEST_POC_VISEO_DB"
   schema              = "RAW_LAYER"
-  url                 =  var.azure_container_url
-  storage_integration = "AZURE_STORAGE_INT"
+  url                 = var.azure_container_url
+  storage_integration = snowflake_storage_integration.azure_int.name
 
-  file_format = "FORMAT_NAME = TEST_POC_VISEO_DB.RAW_LAYER.CSV_FORMAT"
+  file_format = <<-EOT
+    TYPE = CSV
+    SKIP_HEADER = 1
+    FIELD_OPTIONALLY_ENCLOSED_BY = "\""
+  EOT
 }
