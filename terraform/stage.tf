5416c43093e77288b07fad2a03b3f17aa2503633
rihab-lab <rihab.bahri7@gmail.com>
2025-04-24 17:01:36 +0200

    feat: deplyoy infra modern data projets snow

--- terraform/stage.tf
+++ terraform/stage.tf
@@ -1,9 +1,9 @@
 resource "snowflake_storage_integration" "azure_int" {
-  provider              = snowflake.account_admin
-  name                   = "AZURE_STORAGE_INT"
-  type                   = "EXTERNAL_STAGE"
-  storage_provider       = "AZURE"
-  azure_tenant_id        = var.azure_tenant_id
+  provider         = snowflake.account_admin
+  name             = "AZURE_STORAGE_INT"
+  type             = "EXTERNAL_STAGE"
+  storage_provider = "AZURE"
+  azure_tenant_id  = var.azure_tenant_id
   storage_allowed_locations = [
     var.azure_container_url
   ]
@@ -11,22 +11,22 @@ resource "snowflake_storage_integration" "azure_int" {
 }
 
 resource "snowflake_file_format" "csv_format" {
-  provider              = snowflake.account_admin
+  provider    = snowflake.account_admin
   name        = "CSV_FORMAT"
   database    = snowflake_database.db.name
   schema      = snowflake_schema.raw_layer.name
   format_type = "CSV"
 
-  skip_header                   = 1
+  skip_header                  = 1
   field_optionally_enclosed_by = "\""
 }
 
 resource "snowflake_stage" "azure_stage" {
   provider            = snowflake.account_admin
   name                = "EXTERNAL_AZURE_STAGE_BLOB"
-  database    = snowflake_database.db.name
-  schema      = snowflake_schema.raw_layer.name
-  url                 =  var.azure_container_url
+  database            = snowflake_database.db.name
+  schema              = snowflake_schema.raw_layer.name
+  url                 = var.azure_container_url
   storage_integration = "AZURE_STORAGE_INT"
 
   file_format = "FORMAT_NAME = TEST_POC_VISEO_DB.RAW_LAYER.CSV_FORMAT"
